2e716df2f5b3c913d6df12c63fa93fc42290a010
Francesco Iapicca <iapicca@gmail.com>
2023-04-14 18:23:02 +0300

    example cleanup

--- benchmark/just_dart/bin/server.dart
+++ benchmark/just_dart/bin/server.dart
@@ -5,12 +5,12 @@ const _name = 'Just Dart Server';
 
 /// dart compile bin/hello_dart.dart -o ./server
 void main() async {
-  final server = await HttpServer.bind(InternetAddress.anyIPv4, _port);
+  final server = await HttpServer.bind(InternetAddress.anyIPv4, _port)
+    ..serverHeader = _name;
   print('server started: ${server.address} port ${server.port}');
   await for (final request in server) {
     request.response
-      ..headers.set('Server', _name)
-      ..headers.set('Content-Type', 'text/plain')
+      ..headers.set(HttpHeaders.contentTypeHeader, 'text/plain')
       ..write('Hello Dart!')
       ..close();
   }
--- spinda/lib/spinda.dart
+++ spinda/lib/spinda.dart
@@ -1,8 +1,1 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
 library spinda;
-
-export 'src/spinda_base.dart';
-
-// TODO: Export any libraries intended for clients of this package.
